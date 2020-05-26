99efab77ad9b461dda5e31549c370139102792ce
root <root@ip-172-31-50-112.ec2.internal>
2020-05-26 10:29:22 +0000

    UpdateImjage

--- test.js
+++ test.js
@@ -4,7 +4,9 @@ const puppeteer = require('puppeteer');
 
 const browser = await puppeteer.launch({executablePath: '/usr/bin/google-chrome-stable',headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
 const page = await browser.newPage();
-await page.goto('https://www.amazon.com/shop/thehooverboys');
-await page.screenshot({ path: 'screenshot.png', fullPage: true });
+await page.goto('https://www.amazon.com/shop/purejoyhome');
+await page.screenshot({ path: 'purejoyhome.png', fullPage: true });
+await page.goto('https://www.example.com');
+await page.screenshot({ path: 'example.png', fullPage: true});
 browser.close();
 })();
